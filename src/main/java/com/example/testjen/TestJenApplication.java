f36005180e15cd51f53b1c95a7c35880a473af16
Chetan Sharma <Chetan.sharma19971@gmail.com>
2023-10-27 23:11:39 +0530

    init

---
+++ src/main/java/com/example/testjen/TestJenApplication.java
@@ -0,0 +1,13 @@
+package com.example.testjen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testjen/TestJenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testjen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
